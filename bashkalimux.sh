b9b91dffa0118053bc141664de8da7d5cf721a86
RC Chuah <44928288+rc-chuah@users.noreply.github.com>
2021-11-18 22:27:02 +0800

    Update bashkalimux.sh

--- bashkalimux.sh
+++ bashkalimux.sh
@@ -7,28 +7,28 @@
 
 # Menu Choices
 function full() {
-      bash -c "cd ${HOME} && $(curl -fsSL https://bit.do/fNyso) && rm -rf ${HOME}/kalifs-armhf-full.tar.xz && rm -rf ${HOME}/kalifs-arm64-full.tar.xz && rm -rf ${HOME}/kalifs-armhf-full.sha512sum && rm -rf ${HOME}/kalifs-arm64-full.sha512sum"
+     bash -c "cd ${HOME} && $(curl -fsSL https://bit.do/fNyso) && rm -rf ${HOME}/kalifs-armhf-full.tar.xz && rm -rf ${HOME}/kalifs-arm64-full.tar.xz && rm -rf ${HOME}/kalifs-armhf-full.sha512sum && rm -rf ${HOME}/kalifs-arm64-full.sha512sum"
 }
 
 function minimal() {
-      bash -c "cd ${HOME} && $(curl -fsSL https://bit.do/fNysW) && rm -rf ${HOME}/kalifs-armhf-minimal.tar.xz && rm -rf ${HOME}/kalifs-arm64-minimal.tar.xz && rm -rf ${HOME}/kalifs-armhf-minimal.sha512sum && rm -rf ${HOME}/kalifs-arm64-minimal.sha512sum"
+     bash -c "cd ${HOME} && $(curl -fsSL https://bit.do/fNysW) && rm -rf ${HOME}/kalifs-armhf-minimal.tar.xz && rm -rf ${HOME}/kalifs-arm64-minimal.tar.xz && rm -rf ${HOME}/kalifs-armhf-minimal.sha512sum && rm -rf ${HOME}/kalifs-arm64-minimal.sha512sum"
 }
 
 function uninstall() {
-      bash -c "rm -rf ${HOME}/kali-arm64 && rm -rf ${HOME}/kali-armhf && rm -rf ${PREFIX}/bin/nh && rm -rf ${PREFIX}/bin/nethunter && sleep 1 && echo [+] Successfully Uninstalled ..."
+     bash -c "rm -rf ${HOME}/kali-arm64 && rm -rf ${HOME}/kali-armhf && rm -rf ${PREFIX}/bin/nh && rm -rf ${PREFIX}/bin/nethunter && sleep 1 && echo [+] Successfully Uninstalled ..."
 }
 
 # Press Enter To Continue Function
 function press_enter() {
-    echo ""
-    echo -n "Press Enter To Continue..."
-    read
-    clear
+     echo ""
+     echo -n "Press Enter To Continue..."
+     read
+     clear
 }
 
 # Incorrect Selection Function
 function incorrect_selection() {
-    echo "Incorrect Selection! Try Again."
+     echo "Incorrect Selection! Try Again."
 }
 
 # ===================
@@ -36,13 +36,13 @@ function incorrect_selection() {
 # ===================
 
 # Usage Menu
-until [[ "$selection" = "4" ]]; do
+until [[ "$choice" = "4" ]]; do
      clear
      echo "Kali-Nethunter-In-Termux Installer"
      echo ""
      echo "Script By RC Chuah"
      echo ""
-     echo "Current Date And Time :"
+     echo "Current Date And Time:"
      date
      echo ""
      echo "[1]-Install Kali Nethunter In Termux Full Version"
@@ -50,10 +50,9 @@ until [[ "$selection" = "4" ]]; do
      echo "[3]-Uninstall Kali Nethunter In Termux"
      echo "[4]-Exit Menu"
      echo ""
-     echo -n "Enter Selection: "
-     read selection
-     echo ""
-     case $selection in
+     echo -n "Enter Choice: "
+     read choice
+     case $choice in
           1 ) clear ; full ; press_enter ;;
           2 ) clear ; minimal ; press_enter ;;
           3 ) clear ; uninstall ; press_enter ;;
